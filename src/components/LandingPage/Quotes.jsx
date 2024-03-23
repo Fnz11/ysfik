6895c27fa5165ac1ff1239d8eb29d3a8f60fb682
ysFik <finz1112@gmail.com>
2024-03-24 06:28:44 +0700

    Add rocket animation

--- src/components/LandingPage/Quotes.jsx
+++ src/components/LandingPage/Quotes.jsx
@@ -32,6 +32,21 @@ const Quotes = () => {
               className="w-full object-cover group-hover:scale-[1.2] transition-all group-hover:-rotate-1 rotate-1 scale-[1.1] duration-1000 absolute h-[80vh] z-[2]"
               alt=""
             />
+            <div className="absolute z-[3] translate-x-[-5rem] translate-y-[10rem] rotate-[-40deg] group-hover:rotate-0 group-hover:translate-x-0 group-hover:translate-y-0 transition-all duration-[2000ms] top-[55%]  left-[20%] ">
+              <div className="relative w-full h-full floating-element">
+                <Image
+                  width={500}
+                  height={500}
+                  src="/Rocket.png"
+                  className="w-14 opacity-1 blur-[0.02rem] scale-x-[-1] aspect-square "
+                  alt="Rocket"
+                />
+                <div className="scale-[1.5] fire-element absolute top-[2.3rem] left-[-12.5rem]">
+                  <div className="opacity-[0.4] scale-y-[0.5]  rotate-3 bg-gradient-to-bl from-pink-500 to-[80%] blur-[0.9rem] to-transparent h-[10rem] w-[10rem]"></div>
+                </div>
+                <div className="opacity-[0.2]  bg-gradient-to-bl from-purple-500  absolute left-[-7.5rem] top-[1rem] scale-[1.2]  blur-[1rem] to-transparent h-[10rem] w-[10rem]"></div>
+              </div>
+            </div>
             <div className="w-full h-full bg-[rgba(27,5,27,0.6)] backdrop-blur-md group-hover:backdrop-blur-0 transition-all duration-300 z-[2] absolute"></div>
             <div className="z-[4] w-[80%] mx-auto h-full flex flex-col items-center justify-center">
               <h1 className="text-3xl md:text-7xl md:leading-[5rem] mb-10 font-bold drop-shadow-xl text-center">
